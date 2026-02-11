9eabe256430828a55a7152be7f9cace8fb36fecf
YaGraze <gr4zeeh@gmail.com>
2026-02-11 10:51:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import discord
+import os # Библиотека для работы с системой
 from discord.ext import commands
+from dotenv import load_dotenv # Библиотека для загрузки .env файла
+load_dotenv()
 
-# Настройка намерений (Intents)
+TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Событие запуска
 @bot.event
 async def on_ready():
     print(f'Бот вошел в систему как {bot.user}')
 
-# Простая команда !ping
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
 
-# Команда !hello
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Привет, {ctx.author.mention}!')
-
-# ЗАПУСК БОТА
-# Замени 'ТВОЙ_ТОКЕН' на токен, который ты скопировал на Шаге 1
-bot.run('MTEwMzM1MTEwMTkyOTAzMzg1OA.GyOPbu.otkMQIKX4NxbjTaCLSCkVvyGuYnXOMsNoMDX0k')
+if TOKEN is None:
+    print("Ошибка: Токен не найден! Проверь файл .env")
+else:
+    bot.run(TOKEN)
